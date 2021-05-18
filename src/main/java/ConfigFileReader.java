19f3baeccab07212aa82ae1db6380058bd6805d1
Sasha Tuchkovskyi <oleksandr.tuchkovskyi@gmail.com>
2021-05-19 00:03:49 +0300

    Config class

---
+++ src/main/java/ConfigFileReader.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigFileReader {
+    private Properties properties;
+    private final String propertyFilePath= "src/main/resources/configuration.properties";
+
+
+
+    public ConfigFileReader(){
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(propertyFilePath));
+            properties = new Properties();
+            try {
+                properties.load(reader);
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Configuration.properties not found at " + propertyFilePath);
+        }
+    }
+
+    public String getApplicationUrl() {
+        String url = properties.getProperty("url");
+        if(url != null) return url;
+        else throw new RuntimeException("url not specified in the Configuration.properties file.");
+    }
+}
